cd5a9411bdbf9bb9a4ec6b0cc594af6bd3c80082
Maximilian Weißbacher <ma.weissbacher@hak-kitz.ac.at>
2017-12-01 08:00:24 +0100

    commit 2

--- Stress/Stress/Form1.Designer.cs
+++ Stress/Stress/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(113, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(12, 53);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(176, 20);
+            this.txt_username.TabIndex = 1;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(12, 98);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(176, 20);
+            this.txt_pass.TabIndex = 2;
+            this.txt_pass.UseSystemPasswordChar = true;
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_username.Location = new System.Drawing.Point(13, 34);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(55, 13);
+            this.lbl_username.TabIndex = 3;
+            this.lbl_username.Text = "Username";
+            // 
+            // lbl_pass
+            // 
+            this.lbl_pass.AutoSize = true;
+            this.lbl_pass.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_pass.Location = new System.Drawing.Point(13, 82);
+            this.lbl_pass.Name = "lbl_pass";
+            this.lbl_pass.Size = new System.Drawing.Size(53, 13);
+            this.lbl_pass.TabIndex = 4;
+            this.lbl_pass.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = global::Stress.Properties.Resources.wall1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(259, 198);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.lbl_username);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Password Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.Label lbl_pass;
     }
 }
 
