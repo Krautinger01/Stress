3ac498dc89596e7afa24c20e558b8c67a984880d
Marcel Riedmann <marcel.riedmann@hak-kitz.ac.at>
2017-12-01 08:56:17 +0100

    commit 8 erste schritte zur anmeldung

--- Stress/Stress/Form1.cs
+++ Stress/Stress/Form1.cs
@@ -17,6 +17,20 @@ namespace Stress
             InitializeComponent();
         }
 
+        StressEntities entity = new StressEntities();
+
+        private bool CheckUser()
+        {
+            foreach(Stress_Login sl in entity.Stress_Login)
+            {
+                if(sl.username == txt_username.Text && sl.password == PasswordHash.Hash(txt_pass.Text))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             
